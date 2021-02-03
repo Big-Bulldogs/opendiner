b63a7dd07937ff19b352bc0783b4af71dafd4617
Samuel Baetz <samuelbaetz105@gmail.com>
2021-02-02 18:50:11 -0600

    npm init

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const cors = require('cors')
+const PORT = process.env.PORT || 3005;
+
+const db = require("./models/schema");
+
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/opendiner", { useNewUrlParser: true });
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
