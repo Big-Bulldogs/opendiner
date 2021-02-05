fd0af9f49bcb8afc9fcc773053dac325fe035842
Thomas Rager <tomrager@yahoo.com>
2021-02-05 10:10:08 -0500

    working on passport module...get route in server.js still needs configured

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const cors = require('cors')
 const PORT = process.env.PORT || 3005;
+import passport from './config/passport';
+var session = require("express-session");
 
 const db = require("./models/schema");
 
@@ -13,6 +15,13 @@ app.use(logger("dev"));
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+//Not sure how to configure this yet
+// app.get("/", function (req, res) { 
+//   res.sendFile(__dirname + "/public/login.html");
+// });
 
 app.use(express.static("public"));
 
