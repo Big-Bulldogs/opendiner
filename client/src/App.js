e564e14c62b4acd48404c0faf9085820d5506400
Thomas Rager <tomrager@yahoo.com>
2021-02-04 20:25:56 -0500

    adding pages in pages folder for signin and sign up as well as changes to the appbar component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Appbar from './components/Appbar'
 
 
+
 function App() {
   return (
     <div className="App">
--- client/src/components/Appbar.js
+++ client/src/components/Appbar.js
@@ -23,6 +23,7 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     display: 'none',
+    fontFamily: ["'Coustard', serif"].join(','),
     [theme.breakpoints.up('sm')]: {
       display: 'block',
     },
@@ -156,14 +157,6 @@ export default function PrimarySearchAppBar() {
     <div className={classes.grow}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
             OpenDiner
           </Typography>
