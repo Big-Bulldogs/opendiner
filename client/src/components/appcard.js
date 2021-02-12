9122733ad7e02ffb83843b14788335e0b1123c25
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-11 20:09:13 -0500

    updating branch to master

--- client/src/components/appcard.js
+++ client/src/components/appcard.js
@@ -4,9 +4,10 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';;
+import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import {connect} from 'react-redux';
+import {updateData} from '../store/actions/fetchRestaurants'
 
 const useStyles = makeStyles({
   root: {
