457fe8ca3612657b1ff4bd7c6e7d1ae4fba86f06
Samuel Baetz <samuelbaetz105@gmail.com>
2021-02-16 17:47:12 -0600

    menu items

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const cors = require('cors')
 const PORT = process.env.PORT || 3005;
 const passport = require('./config/passport');
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('@admin-bro/express')
 var session = require("express-session");
 
 const db = require("./models");
@@ -23,8 +25,15 @@ app.use(passport.session());
 //   res.sendFile(__dirname + "/public/login.html");
 // });
 
-app.use(express.static("public"));
+const adminBro = new AdminBro({
+  databases: [],
+  rootPath: '/admin',
+})
 
+const router = AdminBroExpress.buildRouter(adminBro)
+
+app.use(express.static("public"));
+app.use(adminBro.options.rootPath, router)
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/opendiner", { useNewUrlParser: true });
 mongoose.set('useFindAndModify', false);
 require('./routes/api-routes')(app)
