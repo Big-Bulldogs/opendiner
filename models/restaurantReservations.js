fe0ae13c269b6d150f12313baca91eb1552a089b
Waleed McLean <mr247365366inaleapyear@gmail.com>
2021-02-16 19:22:45 -0500

    added to reservation schema

--- models/restaurantReservations.js
+++ models/restaurantReservations.js
@@ -2,15 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ReservationSchema = new Schema({
-  restaurant: {
+  restaurant: mongoose.Schema({
+
     type: Schema.Types.ObjectId,
     ref: "Restaurant",
-  },
+  }),
   reservationDate: {
       type: Array
   },
   reservationTime: {
       type: Array
   },
+  reservationMenu: {
+    type: Schema.Types.ObjectId,
+    ref: "reservationMenu"
+
+  },
+
 
 });
+
+const Reservation = mongoose.model("reservation", ReservationSchema);
+
+module.exports = Reservation;
