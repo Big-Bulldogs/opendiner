15417dbfd513dd41259ee613a2f34514a6084d74
Samuel Baetz <samuelbaetz105@gmail.com>
2021-02-22 20:39:51 -0600

    login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,68 +1,62 @@
-import {React, useState} from 'react'
+import { useHistory, Link } from 'react-router-dom';
+import { React, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField'
+import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import API from '../utils/API'
-import { useHistory,Link } from 'react-router-dom';
-
-const useStyles = makeStyles((theme) => ({
+import API from '../utils/API';
+const Login = () => {
+  const useStyles = makeStyles(theme => ({
     root: {
       display: 'flex',
       flexWrap: 'wrap',
       '& > *': {
         margin: theme.spacing(1),
         width: theme.spacing(300),
-        height: theme.spacing(16),
-      },
-    },
+        height: theme.spacing(16)
+      }
+    }
   }));
-const Login = () => {
-    const classes = useStyles();
-    const [formObject, setFormObject] = useState({})
-
-    function handleInputChange(event){
-        const {name,value} = event.target;
-        setFormObject({...formObject, [name]:value})
-        
-    }
-    let history = useHistory();
-
-    function handleFormSubmit(event){
-        event.preventDefault()
-        API.login({
-            email: formObject.email,
-            password: formObject.password
-        })
-        .then(res => {
-            console.log(res.data)
-            history.push('/home')
-        })
-        .catch(err => console.log(err))
-    }
-    return (
-<div className={classes.root}>
+  const classes = useStyles();
+  const [formObject, setFormObject] = useState({});
+  const history = useHistory();
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({ ...formObject, [name]: value });
+  }
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    API.login({
+      email: formObject.email,
+      password: formObject.password
+    })
+      .then(res => {
+        console.log(res.data);
+        history.push('/home');
+      })
+      .catch(err => console.log(err));
+  }
+  return (
+    <div className={classes.root}>
       <Paper elevation={3}>
-          <div>
-      <TextField id="standard-basic" onChange={handleInputChange} name="email" label="Email" />
-      </div>
-      <div>
-      <TextField id="standard-basic" onChange={handleInputChange} name="password" label="Password" />
-      </div>
+        <div>
+          <TextField id="standard-basic" onChange={handleInputChange} name="email" label="Email" />
+        </div>
+        <div>
+          <TextField id="standard-basic" onChange={handleInputChange} name="password" label="Password" />
+        </div>
       </Paper>
       <div>
-      <Button variant="contained" onClick={handleFormSubmit} color="primary">
-  Login
-</Button>
-<Link to="/signup">
-<Button variant="contained" color="primary">
-  Sign Up
-</Button>
-</Link>
+        <Button variant="contained" onClick={handleFormSubmit} color="primary">
+          Login
+        </Button>
+        <Link to="/signup">
+          <Button variant="contained" color="primary">
+            Sign Up
+          </Button>
+        </Link>
       </div>
-      
     </div>
-    )
-}
-
-export default Login
+  );
+};
+export default Login;
